4f0e2ce4f46dd0897d98a236be9ef1d52b4251eb
Toteya <111172685+Toteya@users.noreply.github.com>
2025-12-20 12:14:46 +0200

    rearranged files

--- dev_tools/setup_dev_env.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash 
-unset OMAWI_ENV
-export OMAWI_MYSQL_USER="omawi_dev" OMAWI_MYSQL_HOST="localhost" OMAWI_MYSQL_PWD="omawi_dev_pwd" OMAWI_MYSQL_DB="omawi_dev_db" OMAWI_API_HOST="0.0.0.0" OMAWI_API_PORT='5000' FLASK_DEBUG='True'
