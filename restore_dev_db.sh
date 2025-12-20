e9d82b2953de029c5fcd7fa86c0edcffb2bf76f7
Toteya <111172685+Toteya@users.noreply.github.com>
2025-12-20 12:16:35 +0200

    moved backend files to server subfolder

--- restore_dev_db.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-cat dev_tools/init_dev_db.sql | mysql -uroot -p
-mysql -uroot -p omawi_dev_db < ./dev_tools/omawi_dev_dump.sql
--- start_api.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source ./dev_tools/setup_dev_env.sh
-python3 -m api.v1.app
