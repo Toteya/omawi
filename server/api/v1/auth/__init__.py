7e8bac118f5319779479ca17f13e71c55949e46d
Toteya <111172685+Toteya@users.noreply.github.com>
2025-12-29 10:11:44 +0200

    Fixed: blueprint missing API url_prefix

--- server/api/v1/auth/__init__.py
+++ server/api/v1/auth/__init__.py
@@ -4,6 +4,6 @@ Blueprint for flask web app auth routes
 """
 from flask import Blueprint
 
-app_auth = Blueprint('app_auth', __name__)
+app_auth = Blueprint('app_auth', __name__, url_prefix="/api/v1/auth")
 
 from api.v1.auth.session import *
