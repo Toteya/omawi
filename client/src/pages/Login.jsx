e2c2b62f34fe931d9e9da1b1bb12852f91295788
Toteya <111172685+Toteya@users.noreply.github.com>
2025-12-30 12:49:15 +0200

    Added error handling, error messages, and data format validation to Login and Signup pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useAuth } from "../context/AuthContext"
+import { useState } from "react"
 
 export default function Login() {
+  const [loginFailed, setLoginFailed] = useState(false);
   const { login } = useAuth()
   async function handleSubmit(e) {
     e.preventDefault()
@@ -10,36 +12,42 @@ export default function Login() {
       form.get("email"),
       form.get("password"),
       form.get("remember") === "on",
-    )
-    navigation.navigate("/")
+    ).then(() => {
+      navigation.navigate("/")
+    }).catch(() => {
+      setLoginFailed(true)
+    })
   }
   return (
     <div className="Page">
       <h1 className="mb-4">Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form className="bg-white rounded-lg p-4" onSubmit={handleSubmit}>
         <input
           className="mb-4 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          type="text"
+          type="email"
           name="email"
           label="email"
           placeholder="email"
+          required
         />
         <input
-          className="mb-4 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          className="mb-2 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           type="password"
           name="password"
           placeholder="password"
+          required
         />
-        <label className="m-2">
+        {loginFailed && (<p className="text-red-500 text-sm">Invalid login credentials</p>)}
+        <div className="mt-2">
           <input
             className="inline-flex mr-2"
             type="checkbox"
             name="remember"
           />
           Remember me
-        </label>
-        <button className="border" aria-label="login">Login</button>
-        <p> Don't have an account yet? <a href="/signup" className="text-blue-500">Sign up</a></p>
+          <button className="border ml-4" aria-label="login">Login</button>
+        </div>
+        <p className="mt-2"> Don't have an account yet? <a href="/signup" className="text-blue-500 text-nowrap">Sign up</a></p>
       </form>
     </div>
   )
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,57 +1,71 @@
 import { useAuth } from "../context/AuthContext"
+import { useState } from "react"
 
 export default function Signup() {
   const { signup } = useAuth()
+  const [pwdNotMatching, setPwdNotMatching] = useState(false)
+  const [userAlreadyExists, setUserAlreadyExists] = useState(false)
+
   async function handleSubmit(e) {
     e.preventDefault()
     const form = new FormData(e.target)
 
+    setPwdNotMatching(false)
     if (form.get("password") !== form.get("confirm_password")) {
-      alert("Passwords do not match")
+      setPwdNotMatching(true)
       return
     }
 
+    setUserAlreadyExists(false)
     await signup(
       form.get("name"),
       form.get("email"),
       form.get("password"),
-    )
-    navigation.navigate("/")
+    ).then(() => {
+      navigation.navigate("/")
+    }).catch((e) => {
+      if(e.message.includes("409")) {
+        setUserAlreadyExists(true)
+      }
+    })
   }
 
   return (
     <div className="Page">
       <h1 className="mb-4">Sign Up</h1>
-      <form onSubmit={handleSubmit}>
+      <form className="bg-white rounded-lg p-4" onSubmit={handleSubmit}>
         <input
-          className="mb-4 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          className="mb-2 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           type="text"
           name="name"
           placeholder="name"
           required
         />
         <input
-          className="mb-4 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          type="text"
+          className="mb-2 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          type="email"
           name="email"
           placeholder="email"
           required
         />
+        {userAlreadyExists && (<p className="text-red-500 text-sm">An account with that email already exists.</p>)}
         <input
-          className="mb-4 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          className="mt-4 mb-2 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           type="password"
           name="password"
           placeholder="password"
           required
         />
         <input
-          className="mb-4 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          className="my-2 border text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           type="password"
           name="confirm_password"
           placeholder="confirm password"
           required
         />
-        <button className="border" aria-label="login">Register</button>
+        {pwdNotMatching && (<p className="text-red-500 text-sm">Passwords do not match</p>)}
+        <button className="border mt-2" aria-label="login">Register</button>
+        <p className="mt-2"> Already have an account? <a href="/login" className="text-blue-500 text-nowrap">Log in</a></p>
       </form>
     </div>
   )
